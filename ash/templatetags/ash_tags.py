fa0bf681018ce1f9d31c068803d967c01de66718
Mohammad Faraji <farajim25@gmail.com>
2021-05-09 11:37:14 +0430

    fix list result checkbox column width fix fload and decimal field width

--- ash/templatetags/ash_tags.py
+++ ash/templatetags/ash_tags.py
@@ -53,6 +53,11 @@ def ash_form_field_widget_class_setter(widget, **kwargs):
         widget.attrs['class'] = f'{old_class} vLargeTextField {default_new_class}'
         return widget
 
+    if isinstance(widget, FormWidgets.NumberInput):
+        old_class = widget.attrs.get('class', '')
+        widget.attrs['class'] = f'{old_class} vNumberField {default_new_class}'
+        return widget
+
     if isinstance(widget, AdminWidgets.AdminRadioSelect):
         return widget
 
